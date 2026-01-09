04cb46bfceb1a950954daa017402b0ebc7d7d4ca
Aidan Bush <aidan.brock.mine@outlook.com>
2026-01-09 14:43:31 +0000

    made inital script for reading in excel spreadsheets. This will need changing once sample sheet is recieved

---
+++ backend/src/excel-reader.ts
@@ -0,0 +1,52 @@
+import Excel from 'exceljs';
+
+
+const NAME_INDEX = 1;
+const RESOURCE_BU_INDEX = 2;
+const CUSTOMER_INDEX = 3;
+const UNKNOWN_FIELD_INDEX = 7;
+const JOB_CODE_INDEX = 8;
+const DESCRIPTION_INDEX = 9;
+const RESOURCE_ALLOCATION_RANGE = [10,16];
+
+
+export default async function excelToJSON(excelFileBase64: string) {
+    const excelFileBuffer = Buffer.from(excelFileBase64, 'base64');
+
+    const workbook = new Excel.Workbook();
+    //@ts-expect-error
+    await workbook.xlsx.load(excelFileBuffer); //For some reason there is a type incompatibility here 
+    const worksheet = workbook.worksheets[0];
+
+    let currentRow = 20;
+    let row = worksheet.getRow(20);
+
+    //TODO: get proper stop condition
+    let parsedExcelInfo = [];
+    while (row.getCell(1).value != '') {
+
+        parsedExcelInfo.push({
+            name                : row.getCell(NAME_INDEX).value,
+            resource_bu         : row.getCell(RESOURCE_BU_INDEX).value,
+            customer            : row.getCell(CUSTOMER_INDEX).value,
+            unknown             : row.getCell(UNKNOWN_FIELD_INDEX).value,
+            job_code            : row.getCell(JOB_CODE_INDEX).value,
+            description         : row.getCell(DESCRIPTION_INDEX).value,
+            resource_allocation : getResourceAllocationArray(row)
+        });
+
+        //TODO: get proper increment amount
+        row = worksheet.getRow(++currentRow);
+    } 
+
+    return parsedExcelInfo;
+}
+
+function getResourceAllocationArray(row: Excel.Row) {
+    const [start, end] = RESOURCE_ALLOCATION_RANGE;
+    const allocation = [];
+    for (let col = start; col <= end; col++) {
+        allocation.push(row.getCell(col).value);
+    }
+    return allocation;
+}

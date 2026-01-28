79dbf820c841e30456cf62af5fcab9b3208a3a0a
Aidan Bush <aidan.brock.mine@outlook.com>
2026-01-28 17:07:01 +0000

    perf: Moved parsedExcelInfo declaration to parseExcelInfo.ts

--- backend/src/excel-utils/parsedExcelInfo.ts
+++
@@ -1,9 +0,0 @@
-export default interface parsedExcelInfo {
-    name: string | undefined, 
-    resource_bu: string | undefined, 
-    customer: string | undefined, 
-    unknown: string | undefined, 
-    jobcode: string | undefined, 
-    description: string | undefined, 
-    resource_allocation: (string | undefined)[]
-}

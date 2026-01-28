e222a2ccc596bd9eee70e06a73687ebaae9b76d8
Aidan Bush <aidan.brock.mine@outlook.com>
2026-01-28 17:05:52 +0000

    feat: created inital version of the excel parser Created excel parser script that will run on a fs.ReadStream type.
    This still needs to be commented

--- backend/src/excel-utils/parseExcel.ts
+++ backend/src/excel-utils/parseExcel.ts
@@ -1,54 +1,79 @@
 import Excel from 'exceljs';
-import parsedExcelInfo from './parsedExcelInfo';
+import fs from 'node:fs'
+
+export interface parsedExcelInfo {
+    name                    : string, 
+    resource_bu             : string, 
+    customer                : string, 
+    reply_entity            : string, 
+    bussiness_unit          : string,
+    job_origin              : string,
+    t_code                  : string,
+    job_code                : string, 
+    description             : string, 
+    resource_allocation     : (string)[]
+}
 
 
-const NAME_INDEX = 1;
-const RESOURCE_BU_INDEX = 2;
-const CUSTOMER_INDEX = 3;
-const UNKNOWN_FIELD_INDEX = 7;
-const JOB_CODE_INDEX = 8;
-const DESCRIPTION_INDEX = 9;
-const RESOURCE_ALLOCATION_RANGE = [10,16];
+const NAME_INDEX                = 1;
+const RESOURCE_BU_INDEX         = 2;
+const CUSTOMER_INDEX            = 3;
+const REPLY_ENTITY_INDEX        = 4;
+const BUSSINESS_UNIT_INDEX      = 5;
+const JOB_ORIGIN_INDEX          = 6;
+const T_CODE                    = 8;
+const JOB_CODE_INDEX            = 9;
+const DESCRIPTION_INDEX         = 10;
+const RESOURCE_ALLOCATION_RANGE = [11,17];
 
 
-
-export default async function excelToJSON(excelFileBase64: string) {
-    const excelFileBuffer = Buffer.from(excelFileBase64, 'base64');
-
+export default async function parseExcelInfo(excelFileStream: fs.ReadStream) : Promise<parsedExcelInfo[]> {
+    
     const workbook = new Excel.Workbook();
-    //@ts-expect-error
-    await workbook.xlsx.load(excelFileBuffer); //For some reason there is a type incompatibility here 
+    await workbook.xlsx.read(excelFileStream);
     const worksheet = workbook.worksheets[0];
 
     let currentRow = 20;
     let row = worksheet.getRow(20);
 
-    //TODO: get proper stop condition
     let parsedExcelInfo: parsedExcelInfo[] = [];
-    while (row.getCell(1).value != '') {
+    while (getCellString(row.getCell(2)) != "") {
+
+        if (getCellString(row.getCell(2)).startsWith("TOTAL")) {
+            row = worksheet.getRow(++currentRow);
+            continue;
+        }
 
         parsedExcelInfo.push({
-            name                : row.getCell(NAME_INDEX).value?.toString(),
-            resource_bu         : row.getCell(RESOURCE_BU_INDEX).value?.toString(),
-            customer            : row.getCell(CUSTOMER_INDEX).value?.toString(),
-            unknown             : row.getCell(UNKNOWN_FIELD_INDEX).value?.toString,
-            job_code            : row.getCell(JOB_CODE_INDEX).value?.toString(),
-            description         : row.getCell(DESCRIPTION_INDEX).value?.toString(),
+            name                : getCellString(row.getCell(NAME_INDEX)),
+            resource_bu         : getCellString(row.getCell(RESOURCE_BU_INDEX)),
+            customer            : getCellString(row.getCell(CUSTOMER_INDEX)),
+            reply_entity        : getCellString(row.getCell(REPLY_ENTITY_INDEX)),
+            bussiness_unit      : getCellString(row.getCell(BUSSINESS_UNIT_INDEX)),
+            job_origin          : getCellString(row.getCell(JOB_ORIGIN_INDEX)),
+            t_code              : getCellString(row.getCell(T_CODE)),
+            job_code            : getCellString(row.getCell(JOB_CODE_INDEX)),
+            description         : getCellString(row.getCell(DESCRIPTION_INDEX)),
             resource_allocation : getResourceAllocationArray(row)
         });
 
-        //TODO: get proper increment amount
         row = worksheet.getRow(++currentRow);
     } 
 
     return parsedExcelInfo;
 }
 
-function getResourceAllocationArray(row: Excel.Row) : (string | undefined)[] {
+// Returns an empty string if the value would be undefined
+function getCellString(cell: Excel.Cell) : string {
+    let cellString: string|undefined = cell.value?.toString();
+    return cellString === undefined ? "" : cellString;
+}
+
+function getResourceAllocationArray(row: Excel.Row) : (string)[] {
     const [start, end] = RESOURCE_ALLOCATION_RANGE;
     const allocation = [];
     for (let col = start; col <= end; col++) {
-        allocation.push(row.getCell(col).value?.toString());
+        allocation.push(getCellString(row.getCell(col)));
     }
     return allocation;
 }

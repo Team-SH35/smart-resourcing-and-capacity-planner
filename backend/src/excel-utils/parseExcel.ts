0c8c9a6b7a12eb52154ffda32b1de36791696ecd
Aidan Bush <aidan.brock.mine@outlook.com>
2026-02-04 15:17:03 +0000

    feat!: excel parser now read normal and work days

--- backend/src/excel-utils/parseExcel.ts
+++ backend/src/excel-utils/parseExcel.ts
@@ -1,7 +1,28 @@
 import Excel from 'exceljs';
 import fs from 'node:fs'
 
-export interface parsedExcelRow {
+
+interface MonthAllocations {
+    jan : MonthAllocation,
+    feb : MonthAllocation,
+    mar : MonthAllocation,
+    apr : MonthAllocation,
+    may : MonthAllocation,
+    jun : MonthAllocation,
+    jul : MonthAllocation,
+    aug : MonthAllocation,
+    sep : MonthAllocation,
+    oct : MonthAllocation,
+    nov : MonthAllocation,
+    dec : MonthAllocation,
+}
+
+interface MonthAllocation {
+    HYPO: number | null,
+    work: number | null,
+}
+
+interface parsedExcelRow {
     name                    : string, 
     resource_bu             : string, 
     customer                : string, 
@@ -12,6 +33,11 @@ export interface parsedExcelRow {
     job_code                : string, 
     description             : string, 
     resource_allocation     : (string)[]
+}
+
+export interface ParsedExcelInfo {
+    allocation_days : MonthAllocations
+    rows : parsedExcelRow[]
 }
 
 
@@ -27,17 +53,24 @@ const DESCRIPTION_INDEX         = 10;
 const RESOURCE_ALLOCATION_RANGE = [11,17];
 
 
-export default async function parseExcelInfo(excelFileStream: fs.ReadStream) : Promise<parsedExcelRow[]> {
+export default async function parseExcelInfo(excelFileStream: fs.ReadStream) : Promise<ParsedExcelInfo> {
     
     const workbook = new Excel.Workbook();
     await workbook.xlsx.read(excelFileStream);
     const worksheet = workbook.worksheets[0];
 
+    let parsedExcelData: ParsedExcelInfo = {
+        allocation_days: getMonthAllocationDays(worksheet),
+        rows: []
+    };
+
+    parsedExcelData.allocation_days = getMonthAllocationDays(worksheet);
+
+    
     // Row 20 is where the first row of forecast data is
     let currentRow = 20;
     let row = worksheet.getRow(currentRow);
 
-    let parsedExcelInfo: parsedExcelRow[] = [];
     while (getCellString(row.getCell(2)) != "") {
 
         // If at total then at end of current employee. Go to next line
@@ -46,7 +79,7 @@ export default async function parseExcelInfo(excelFileStream: fs.ReadStream) : P
             continue;
         }
 
-        parsedExcelInfo.push({
+        parsedExcelData.rows.push({
             name                : getCellString(row.getCell(NAME_INDEX)),
             resource_bu         : getCellString(row.getCell(RESOURCE_BU_INDEX)),
             customer            : getCellString(row.getCell(CUSTOMER_INDEX)),
@@ -62,7 +95,7 @@ export default async function parseExcelInfo(excelFileStream: fs.ReadStream) : P
         row = worksheet.getRow(++currentRow);
     } 
 
-    return parsedExcelInfo;
+    return parsedExcelData;
 }
 
 // Returns an empty string if the cell value would be undefined
@@ -78,4 +111,37 @@ function getResourceAllocationArray(row: Excel.Row) : (string)[] {
         allocation.push(getCellString(row.getCell(col)));
     }
     return allocation;
+}
+
+// Reads HYPO and normal work days from heading of excel sheet
+function getMonthAllocationDays(worksheet: Excel.Worksheet) {
+    const [start, end] = RESOURCE_ALLOCATION_RANGE;
+
+    const row = worksheet.getRow(19);
+    const workDays: MonthAllocation[] = [];
+
+    for (let col = start; col <= end; col++) {
+        const month = getCellString(row.getCell(col));
+        const match = month.match(/(\d+)\/(\d+)/);
+
+        if (!match) {
+            workDays.push({HYPO : null, work: null});
+        } else {
+            workDays.push({HYPO: Number(match[1]), work: Number(match[2])});
+        }
+    }
+    return {
+        jan : workDays[0],
+        feb : workDays[1],
+        mar : workDays[2],
+        apr : workDays[3],
+        may : workDays[4],
+        jun : workDays[5],
+        jul : workDays[6],
+        aug : workDays[7],
+        sep : workDays[8],
+        oct : workDays[9],
+        nov : workDays[10],
+        dec : workDays[11],
+    }
 }

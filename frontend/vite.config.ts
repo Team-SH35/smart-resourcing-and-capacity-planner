27c9ffbc6a812769688cb4c8cbd93339013f555b
2873913s <2873913s@student.gla.ac.uk>
2026-03-15 16:02:08 +0000

    fix(156-frontend-tests-employee-page): Lint Cleanup
    vite.config.g imports define config correctly now

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // Vitest configuration
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/_test_/setup.ts",
   },
-} as any);
+});
